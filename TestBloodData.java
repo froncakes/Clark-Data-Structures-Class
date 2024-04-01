a8680a1e30de593466d0d40d8ef74252390d4c3f
froncakes <75559235+froncakes@users.noreply.github.com>
2024-04-01 04:26:04 -0400

    Delete TestBloodData.java

--- TestBloodData.java
+++
@@ -1,8 +0,0 @@
-public class TestBloodData {
-    public static void main(String[] args) {
-        BloodData a = new BloodData();
-        System.out.println(a.toString());
-        BloodData b = new BloodData(BloodType.AB,"-");
-        System.out.println(b.toString());
-    }
-}
