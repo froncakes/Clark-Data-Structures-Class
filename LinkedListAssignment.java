d8f7fdce2c39915ccad6f50634bcb51684f2728d
froncakes <75559235+froncakes@users.noreply.github.com>
2024-04-06 22:24:57 -0400

    Add files via upload

---
+++ LinkedListAssignment.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class LinkedListAssignment {
+    public static void main(String[] args){
+
+    }
+    // removes the node after the number given
+    public void removeAfter(LinkedList a, int i){
+        if (i < 0 || i >= a.size()-1){
+
+        }
+    }
+    public LinkedList copy(LinkedList a){
+
+    }
+    public void removeDuplicate(LinkedList a, String dup){
+
+    }
+    public void max(LinkedList a){
+
+    }
+}
